594e153471eea0f28842a0d0b6aefc208103d371
Tammy Fina <finatammy@gmail.com>
2021-12-20 17:49:20 -0500

    Generate the HTML File

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+
+
+
+
+
+
+
+
 // console.log(profileDataArgs);
+// const printProfileData = profileDataArr => {
+//     for (let i=0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
+//     console.log('==================');
+//     profileDataArr.forEach((profileItem) => 
+//     console.log(profileItem));
+//     };
+// printProfileData(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    for (let i=0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-
-    console.log('==================');
-
-    profileDataArr.forEach((profileItem) => 
-    console.log(profileItem));
-    };
-printProfileData(profileDataArgs);
 
