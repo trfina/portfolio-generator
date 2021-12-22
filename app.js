3775be8761ec53f1f67c3291844329231fc715ba
Tammy Fina <finatammy@gmail.com>
2021-12-22 15:50:51 -0500

    inquirer prompt added

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-
-const pageHTML = generatePage(name, github);
-
-fs.writeFile('./index.html', pageHTML, err => {
-    if (err) throw new err;
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+const inquirer = require('inquirer');
 
 
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage (name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
 
 
@@ -17,15 +29,6 @@ fs.writeFile('./index.html', pageHTML, err => {
 
 
 
-// console.log(profileDataArgs);
-// const printProfileData = profileDataArr => {
-//     for (let i=0; i < profileDataArr.length; i += 1) {
-//         console.log(profileDataArr[i]);
-//     }
-//     console.log('==================');
-//     profileDataArr.forEach((profileItem) => 
-//     console.log(profileItem));
-//     };
-// printProfileData(profileDataArgs);
+
 
 
