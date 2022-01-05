912b2e09e7f0c6c2efdb1dbfd49d171c2ecfe6ac
Tammy Fina <finatammy@gmail.com>
2022-01-04 22:23:04 -0500

    update template and refactor

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const fs = require('fs');
+// this expression assigns the anonymous HTML template function 
+// in pagetemplate.js to the variable generatePage
+const generatePage = require('./src/page-template');
 
 
 const promptUser = () => {
@@ -140,25 +142,11 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage (portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw err;
+            console.log('Page created! Check out index.html');
+        });
+        
     });
- 
-
-
-// const pageHTML = generatePage (name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
-
-
-
-
-
-
-
-
-
-
